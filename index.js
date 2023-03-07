db41e599726f1fc178d055d026ae0023798267a1
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-06 17:03:41 -0700

    add inquirer .prompt structure ti index.js file and add lib directory with js files for shapes and shapes test

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // Dependencies
 const inquirer = require("inquirer");
-const fs = require("fs");
+const fs = require("fs"); //part of node installations; use to writeFile
+
+const generateSVG = ({
+    
+}) => ``;
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter up to three characters for your logo.",
+      name: "characters",
+    },
+  ])
+  .then((answers) => {
+    const svgContent = generateSVG(answers);
+
+    fs.writeFile("", svgContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created SVG logo!")
+    );
+  });
