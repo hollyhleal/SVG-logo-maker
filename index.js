36b53fb2caa247df491131b75cf929dada7f0af2
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-03-07 16:27:05 -0700

    add rest of user prompts

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs"); //part of node installations; use to writeFile
 
-const generateSVG = ({
-    
-}) => ``;
+const generateSVG = ({}) => ``;
 
 inquirer
   .prompt([
@@ -13,11 +11,29 @@ inquirer
       message: "Enter up to three characters for your logo.",
       name: "characters",
     },
+    {
+      type: "list",
+      message: "What color would you like your logo text to be?",
+      name: "textColor",
+      choices: ["white"],
+    },
+    {
+      type: "list",
+      message: "What shape would you like your logo to be?",
+      name: "shape",
+      choices: ["circle"],
+    },
+    {
+      type: "list",
+      message: "What color would you like your logo to be?",
+      name: "shapeColor",
+      choices: [{ title: "blue", value: "#0000ff" }],
+    },
   ])
   .then((answers) => {
     const svgContent = generateSVG(answers);
 
-    fs.writeFile("", svgContent, (err) =>
-      err ? console.log(err) : console.log("Successfully created SVG logo!")
+    fs.writeFile("logo.svg", svgContent, (err) =>
+      err ? console.log(err) : console.log("Generated logo.svg")
     );
   });
